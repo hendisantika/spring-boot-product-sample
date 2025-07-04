f7119ddfb5ad17c953311553203221b6ad94d4eb
hendisantika <hendisantika@yahoo.co.id>
2025-07-05 06:28:50 +0700

    feat: Add ProductControllerIntegrationTest🫸🌀✏️📗🐧🐳

---
+++ src/test/java/com/example/demo/controller/ProductControllerIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.config.AbstractIntegrationTest;
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerIntegrationTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Product testProduct;
+    private List<Product> productList;
+
+
+}
