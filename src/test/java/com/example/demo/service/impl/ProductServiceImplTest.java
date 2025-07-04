a47da7a1ba84427d64e13611970a0252428aa36a
hendisantika <hendisantika@yahoo.co.id>
2025-07-05 06:22:49 +0700

    feat: Add ProductServiceImplTest🫸🌀✏️📗🐧🐳

---
+++ src/test/java/com/example/demo/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceImplTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    private Product testProduct;
+    private List<Product> productList;
+
+
+}
